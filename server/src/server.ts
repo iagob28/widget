7506cd96d51c2be729322b7842b9864b84221039
iago bortolon <iago_bortolon@hotmail.com>
2022-05-08 15:41:14 -0300

    add port variable

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { routes } from "./routes";
 import cors from "cors";
+import { env } from "process";
 
 const app = express();
 
@@ -14,6 +15,6 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
   console.log("HTTP server running");
 });
