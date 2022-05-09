5a28cb4e7c8cc1873380b2277dd13a22b408c75e
iago bortolon <iago_bortolon@hotmail.com>
2022-05-09 11:50:18 -0300

    spec submit-feedback-use-case fixed

--- server/src/use-cases/submit-feedback-use-case.spec.ts
+++ server/src/use-cases/submit-feedback-use-case.spec.ts
@@ -3,10 +3,7 @@ import { SubmitFeedbackUseCase } from "./submit-feedback-use-case";
 const createFeedbackSpy = jest.fn();
 const sendMailSpy = jest.fn();
 
-const submitFeedback = new SubmitFeedbackUseCase(
-  { create: createFeedbackSpy },
-  { sendMail: sendMailSpy }
-);
+const submitFeedback = new SubmitFeedbackUseCase({ create: createFeedbackSpy });
 
 describe("Submit feedback", () => {
   it("should be able to submit a feedback", async () => {
